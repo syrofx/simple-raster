bf43eb1408c68794d47368f33ba560f65a998753
Syro <network.9961@gmail.com>
2025-05-18 02:32:34 -0600

    Add SDL3 dependency

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) == false) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("SDL Window", 640, 640, SDL_WINDOW_OPENGL);
+    if (!win) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return 0;
+}
